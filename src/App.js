dc8cc760b1e45e8c19d7a76b60a83b0972211181
C0dingForFun <gwibantulini8@gmail.com>
2025-01-13 11:34:02 +0200

    rendring list

--- src/App.js
+++ src/App.js
@@ -14,6 +14,19 @@ const user = {
   imageSize: 190,
 }
 
+const products = [
+  {title: 'Cabbage', id: 1},
+  {title: 'Garlic', id: 2},
+  {title: 'Apple', id: 3}
+]
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
+
 export default function App() {
   return (
     <>
@@ -32,6 +45,8 @@ export default function App() {
           height: user.imageSize
         }}
         />
+        <h3>Products:</h3>
+        <ul>{listItems}</ul>
       </div>
     </>
   );
